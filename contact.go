caef914b608e3c1f0f63d253d4f97063bf774026
diptomondal007 <diptomondal007@gmail.com>
2020-01-11 23:53:56 +0600

    contact struct

---
+++ contact.go
@@ -0,0 +1,13 @@
+package ApiwithGoChi
+
+import "time"
+
+type Contact struct {
+	FirstName   string `json:"first_name" bson:"first_name"`
+	LastName    string `json:"last_name" bson:"last_name"`
+	Email       string `json:"email" bson:"email"`
+	PhoneNumber string `json:"phone_number" bson:"phone_number"`
+	Address     string `json:"address" bson:"address"`
+	Company     string `json:"company" bson:"company"`
+	CreatedOn    time.Time `json:"create_on" bson:"create_on"`
+}
